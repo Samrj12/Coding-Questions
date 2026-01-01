cee0d7adb51cfe9e3c24b418836a38d7e21aa193
Rudraksh Jadhav <67728483+Samrj12@users.noreply.github.com>
2026-01-01 13:35:37 -0500

    Add solution for all possible full binary trees
    Implement a solution to generate all possible full binary trees.

---
+++ bst/All Possible Full Binary Trees.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        if (!memo.containsKey(N)) {
+            List<TreeNode> ans = new LinkedList();
+            if (N == 1) {
+                ans.add(new TreeNode(0));
+            } else if (N % 2 == 1) {
+                for (int x = 1; x < N; x += 2) {
+                    int y = N - 1 - x;
+                    for (TreeNode left: allPossibleFBT(x))
+                        for (TreeNode right: allPossibleFBT(y)) {
+                            TreeNode bns = new TreeNode(0,left,right);
+                            ans.add(bns);
+                        }
+                }
+            }
+            memo.put(N, ans);
+        }
+        return memo.get(N);
+    }
+};
