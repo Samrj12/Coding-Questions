e706207fe63a40c20284bb7c29b4c1f9ab4a20e6
Rudraksh Jadhav <67728483+Samrj12@users.noreply.github.com>
2026-03-03 17:35:17 -0500

    Implement function to remove duplicates from sorted list

---
+++ linked_list/Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode* curr;
+
+        while (head && head->next && head->val == head->next->val) {
+            int val = head->val;
+            while (head && head->val == val) {
+                head = head->next;
+            }
+        }
+        if (!head || !head->next)
+            return head;
+        curr = head;
+        ListNode* prev = head;
+        curr = prev->next;
+        while (curr) {
+            while (curr && curr->next && curr->val == curr->next->val) {
+                int val = curr->val;
+                while (curr && curr->val == val) {
+                    curr = curr->next;
+                }
+            }
+            prev->next = curr;
+            if (curr) {
+
+                prev = curr;
+                curr = prev->next;
+            }
+        }
+        return head;
+    }
+};
